6d6d9e6cb1988988e6985ecc780b4d5c69a87abd
juan carlos valencia osorio <juan_5480@hotmail.es>
2025-01-22 00:00:58 -0500

    Final

--- ApiProveedores/Program.cs
+++ ApiProveedores/Program.cs
@@ -39,9 +39,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthentication();
 app.UseAuthorization();
